6f816afd48b954b3c3f358de3e9dbad377e2bcf6
Olga Luk <olga_ivanova_86@mail.ru>
2019-06-04 03:35:45 +0300

    feat: add project

---
+++ precache-manifest.a1ba017e3cab8849001c2f6948430f12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3806c23ad1a372c275d2c414c24af670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0af358e1a26f101d4496",
+    "url": "/static/css/main.45a4aa39.chunk.css"
+  },
+  {
+    "revision": "b5c276047d8cf398fd5f",
+    "url": "/static/js/2.b534bcf5.chunk.js"
+  },
+  {
+    "revision": "0af358e1a26f101d4496",
+    "url": "/static/js/main.40de3075.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
